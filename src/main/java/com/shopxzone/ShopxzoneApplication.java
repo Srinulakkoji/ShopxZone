02e95e4b73ce0a5a43161410dee0b85b813c2ba0
shree <shreenu.vasu.lakkoji@gmail.com>
2026-02-08 21:44:55 +0530

    project start first commit

---
+++ src/main/java/com/shopxzone/ShopxzoneApplication.java
@@ -0,0 +1,13 @@
+package com.shopxzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopxzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopxzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopxzone/ShopxzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopxzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopxzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
